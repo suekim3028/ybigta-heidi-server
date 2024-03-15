00b26e1644b6f2ab9fc455ba5ee54bd5a94d5f13
Suebin Kim <k.sue3028@gmail.com>
2024-03-16 04:20:25 +0900

    feat: return recom exp and healing programs

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import Depends, FastAPI, HTTPException, Request
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas,recommend_models
+from . import crud, models, schemas
 from .database import SessionLocal, engine
 from fastapi.middleware.cors import CORSMiddleware
 from json import JSONDecodeError
 import logging
-
+from .ml_model.models import recom_exp_healing
+import random
+import pandas as pd 
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -54,12 +56,34 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return schemas.UserResponse(user=schemas.userSchema2Dto(db_user), token="")
 
 
-@app.get("/programs/{user_id}", response_model=schemas.ProgramListResponseDto)
+@app.get("/programs/", response_model=schemas.ProgramListResponseDto)
 def getPrograms(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
-    
+
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    programs = recommend_models.recommend(db_user)
+
+    exp_healing_programs = recom_exp_healing.run(
+        db_user.birth_year, db_user.gender, db_user.job, db_user.has_children
+    )
+
+
+    res_programs = pd.concat([exp_healing_programs])
+
+    res_dtos = []
+
+    for __idx__, row in res_programs.iterrows():
+        res_dtos.append(schemas.ProgramMini(id = row["id"], name=row["name"], rate=3, category=row["category"] ))
+
+    return schemas.ProgramListResponseDto(programs=random.shuffle(res_dtos))
+
+
+@app.get("/program/", response_model=schemas.ProgramListResponseDto)
+def getPrograms(user_id: int, program_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
     logger.info(programs)
     return schemas.ProgramListResponseDto(programs=programs)
